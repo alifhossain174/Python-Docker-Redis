aa972ef62053aee2d4e214b8df419758ab20403a
Fahim <alifhossain174@gmail.com>
2025-10-06 10:01:08 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import redis
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    password="MyS3curePassw0rd!",
+    decode_responses=True,  # return str instead of bytes
+    socket_timeout=5,
+)
+
+# Simple health check
+print("PING ->", r.ping())
+
+# Round-trip
+# r.set("framework", "docker+redis")
+print("Get framework ->", r.get("framework"))
+
+# r.delete("framework")
+# print("OK")
